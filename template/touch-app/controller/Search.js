b0fdb854b96539f666bdb21331cbfea45870f035
Rene Saarsoo <nene@triin.net>
2012-03-27 14:08:59 +0300

    Hide search dropdown when search field cleared.
    Also hide it when no search results.

--- template/touch-app/controller/Search.js
+++ template/touch-app/controller/Search.js
@@ -13,12 +13,27 @@ Ext.define('TouchDocs.controller.Search', {
     },
 
     onSearch: function(field) {
-        var results = Docs.ClassRegistry.search(field.getValue()).slice(0, 10);
+        var results = this.filter(field.getValue());
         Ext.getStore('Search').setData(results);
 
         if (!this.searchDropdown) {
             this.searchDropdown = Ext.create('TouchDocs.view.search.Dropdown');
         }
-        this.searchDropdown.showBy(this.getSearchField());
+
+        if (results.length > 0) {
+            this.searchDropdown.showBy(this.getSearchField());
+        }
+        else {
+            this.searchDropdown.hide();
+        }
+    },
+
+    filter: function(query) {
+        if (query === "") {
+            return [];
+        }
+        else {
+            return Docs.ClassRegistry.search(query).slice(0, 10);
+        }
     }
 });
