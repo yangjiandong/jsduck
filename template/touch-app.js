3e0345e7cd50207952bab6d32169d7e6bf699f24
Nick Poulden <nick@sencha.com>
2012-03-29 16:42:07 -0700

    Correct URLs for Touch app icons / start screen

--- template/touch-app.js
+++ template/touch-app.js
@@ -50,14 +50,14 @@ Ext.application({
     ],
 
     icon: {
-        57: 'resources/icons/Icon.png',
-        72: 'resources/icons/Icon~ipad.png',
-        114: 'resources/icons/Icon@2x.png',
-        144: 'resources/icons/Icon~ipad@2x.png'
+        57: 'resources/touch-icons/Icon.png',
+        72: 'resources/touch-icons/Icon~ipad.png',
+        114: 'resources/touch-icons/Icon@2x.png',
+        144: 'resources/touch-icons/Icon~ipad@2x.png'
     },
 
-    phoneStartupScreen: 'resources/loading/Homescreen.jpg',
-    tabletStartupScreen: 'resources/loading/Homescreen~ipad.jpg',
+    phoneStartupScreen: 'resources/touch-loading/Homescreen.jpg',
+    tabletStartupScreen: 'resources/touch-loading/Homescreen~ipad.jpg',
 
     launch: function() {
         // Destroy the #appLoadingIndicator element
